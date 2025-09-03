f5aeb7c5220ffe14708e6577227a86ec7b298eb4
Jayanta Dutta <Jayanta.Dutta@towerswatson.com>
2025-09-03 19:25:13 +0530

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,18 @@
+﻿using ModelContextProtocol.Server;
+using System;
+using System.ComponentModel;
+using System.Linq;
+namespace MCPApp
+{
+    [McpServerToolType] // this tells MCP to scan for tools here
+    public static class Calculator
+    {
+        [McpServerTool, Description("Echoes the message back to the client.")]
+        public static string Echo(string message) => $"Hello from C#: {message}";
+
+        [McpServerTool, Description("Echoes in reverse the message sent by the client.")]
+        public static string ReverseEcho(string message) => new string(message.Reverse().ToArray());
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace MCPApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+            builder.Logging.AddConsole(options =>  options.LogToStandardErrorThreshold = LogLevel.Trace);
+            builder.Services.AddMcpServer().WithStdioServerTransport().WithToolsFromAssembly();
+            await builder.Build().RunAsync();
+        }
+    }
+}
